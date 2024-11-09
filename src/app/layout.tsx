0cf8edb5799f6da144b5b15e7859a77f077a9e55
neo <thetimblank@gmail.com>
2024-11-09 15:09:58 -0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,22 +5,9 @@ import '@/styling/globals.css';
 import Animations from '@/lib/animations/lazy';
 
 export const metadata: Metadata = {
-	title: "neotap: Tim's Portfolio - Full Stack Web Developer & Programmer",
-	description:
-		"A professional portfolio focused on me, Tim Blank. I specialize in making custom web apps and websites, and I have coding experience in TypeScript, Java, Python, Rust. I'm well-versed in frameworks like React and Next.js. Explore my projects to see my expertise in action.",
-	keywords: [
-		'full stack, front end, backend developer',
-		'software engineer portfolio to hire',
-		'web developer',
-		'portfolio',
-		'creative coder',
-		'programmer',
-		'Tim Blank',
-		'web development',
-		'unique projects',
-		'typescript',
-		'rust nextjs react developer',
-	],
+	title: 'neotap.net',
+	description: 'neotap.net',
+	keywords: ['neotap.net'],
 };
 
 const font = Mukta({ weight: ['400', '800'], subsets: ['latin'] });
